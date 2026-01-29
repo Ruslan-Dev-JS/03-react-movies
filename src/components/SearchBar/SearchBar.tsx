9f5b50badd78fbbbe9d62d9a6f28774ccb71f6bb
RuslanLynnyk <ruslynnyk0001@gmail.com>
2026-01-29 08:24:39 +0200

    Fixed error

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -5,11 +5,9 @@ interface SearchBarProps {
   onSubmit: (query: string) => void;
 }
 
-// Цей компонент імітує Form Actions API
 export default function SearchBar({ onSubmit }: SearchBarProps) {
-  // Функція, яка приймає FormData як аргумент
-  const handleAction = (formData: FormData) => {
-    const query = (formData.get("query") as string)?.trim();
+  function action(formData: FormData) {
+    const query = formData.get("query")?.toString().trim();
 
     if (!query) {
       toast.error("Please enter your search query.");
@@ -17,35 +15,18 @@ export default function SearchBar({ onSubmit }: SearchBarProps) {
     }
 
     onSubmit(query);
-  };
+  }
 
   return (
     <header className={css.header}>
       <div className={css.container}>
-        <a
-          className={css.link}
-          href="https://www.themoviedb.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by TMDB
-        </a>
-        {/* Замість onSubmit ставимо action */}
-        <form
-          className={css.form}
-          action={(e: Event) => {
-            e.preventDefault();
-            const formData = new FormData(e.currentTarget as HTMLFormElement);
-            handleAction(formData);
-          }}
-        >
+        <form className={css.form} action={action}>
           <input
             className={css.input}
             type="text"
             name="query"
-            autoComplete="off"
             placeholder="Search movies..."
-            autoFocus
+            autoComplete="off"
           />
           <button className={css.button} type="submit">
             Search
